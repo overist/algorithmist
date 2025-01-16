dea84e40e14df4cdf90c06a395ad6f12b36ab687
overist <wj@over.ist>
2025-01-16 17:25:21 +0900

    feat: 파이썬 템플릿 변경경

--- template.py
+++ template.py
@@ -1,29 +1,25 @@
 import sys
 
 # ** Utilities ---------------------
-def getElementList(input): return input.split()
-def getNumberList(input): return [int(character) for character in input.split()]
-def getLineList(input): return input.split("\n")
-def getMatrix(lineList): return [getNumberList(line) for line in lineList]
+# def getElementList(input): return input.split()
+# def getNumberList(input): return [int(character) for character in input.split()]
+# def getLineList(input): return input.split("\n")
+# def getMatrix(lineList): return [getNumberList(line) for line in lineList]
 # ----------------------------------
 
 # ** Function Block ----------------
-
 # solution ---
-def solution(input):
-    lineList = getLineList(input)[1:]
-    matrix = getMatrix(lineList)
-    return matrix
+def solution():
+    n = sys.stdin.readline().strip()
+    xList = [int(x) for x in sys.stdin.readlines()]
+    return n
 # ----------------------------------
 
 # ** Main --------------------------
-# Preprocess input
-input = """1 3
-2 4 5
-3 4 7""" if sys.stdin.isatty() else sys.stdin.read().strip()
-
-# Run solution
 if __name__ == "__main__":
-    result = solution(input)
-    print(result)
+    if sys.stdin.isatty() :
+        with open('input.txt', "r") as f :
+            sys.stdin = f
+            solution()
+    else : solution()
 # ----------------------------------
